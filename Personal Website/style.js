f19c77b793a7f0e917753592934919281d076bae
tomrogers02 <108933448+tomrogers02@users.noreply.github.com>
2023-01-11 14:40:31 +0000

    Fixes to javascript light/dark/ nav bar
    Couldnt get the colours right fotr the light theme of the website.
    Repsonsive flex box issues with nav bar
    Created 3d avatar of myself and logo

--- Personal Website/style.js
+++ Personal Website/style.js
@@ -1,6 +1,18 @@
 const toggle = document.getElementById('toggle-dark');
 const body = document.querySelector('body');
-const link = document.getElementsByClassName('nav-links')
+const navStickyBar = document.querySelector('header');
+var navLinks = document.getElementsByClassName("nav-links");
+const heroSecondTitle = document.getElementById('upper-title');
+const heroThirdTitle = document.getElementById('lower-subtitle');
+var socialLinksHero = document.getElementsByClassName('bi');
+const leftHeroLine = document.getElementById('social-line');
+const emailAddress = document.getElementById('email');
+const navBottom = document.getElementById('nav-mobile-bottom');
+const navMiddle = document.getElementById('nav-right');
+const navTop = document.getElementById('nav-left');
+
+
+
 
 
 toggle.addEventListener('click', function () {
@@ -9,9 +21,27 @@ toggle.addEventListener('click', function () {
   if (this.classList.toggle('bi-brightness-high-fill')) {
     body.style.background = '#f5f5f5';
     body.style.color = 'black';
-    body.style.a.color = 'black';
     body.style.transition = '2s';
-    link.style.color = 'black';
+    navStickyBar.style.background = '#f5f5f5';
+    heroSecondTitle.style.color = ' black'
+    heroThirdTitle.style.color = 'black';
+    emailAddress.style.color = 'black';
+    navBottom.style.background = '#E5E5E5';
+    navMiddle.style.background = '#F5F5F5';
+    navTop.style.background = '#f5f5f5';
+
+
+    for (var i = 0; i < navLinks.length; i++) {
+      navLinks[i].style.color = "black";
+    }
+
+    for (var i = 0; i < socialLinksHero.length; i++) {
+      socialLinksHero[i].style.color = "black";
+    }
+    s
+
+
+
 
 
 
@@ -19,6 +49,23 @@ toggle.addEventListener('click', function () {
     body.style.background = '#222229';
     body.style.color = 'white';
     body.style.transition = '2s';
+
+    navStickyBar.style.background = '#222229';
+    heroSecondTitle.style.color = ' #c6c6c6'
+    heroThirdTitle.style.color = '#c6c6c6';
+    emailAddress.style.color = 'white'
+    navBottom.style.background = '#45455a';
+    navMiddle.style.background = '#222229';
+    navTop.style.background = '#222229';
+    toggle.style.color = 'white'
+
+    for (var i = 0; i < navLinks.length; i++) {
+      navLinks[i].style.color = "white";
+    }
+
+    for (var i = 0; i < socialLinksHero.length; i++) {
+      socialLinksHero[i].style.color = "white";
+    }
   }
 });
 
