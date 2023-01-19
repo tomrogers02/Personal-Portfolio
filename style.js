3f7507a1cbeff3ac28669e6c0570dd41ecb36d19
Tom Rogers <108933448+tomrogers02@users.noreply.github.com>
2023-01-19 16:03:38 +0000

    Reupload

--- style.js
+++ style.js
@@ -1,42 +1,49 @@
 const toggle = document.getElementById('toggle-dark');
 const body = document.querySelector('body');
 const navStickyBar = document.querySelector('header');
-const navLinks = document.querySelectorAll(".nav-links");
-const heroSecondTitle = document.getElementById('upper-title');
-const heroThirdTitle = document.getElementById('lower-subtitle');
-const socialLinksHero = document.querySelectorAll('.bi');
-const leftHeroLine = document.getElementById('social-line');
+const navLeft = document.getElementById('nav-left');
+const heroTitles = document.querySelectorAll('#upper-title, #lower-subtitle');
+const navLinks = document.querySelectorAll(".nav-links, .bi");
 const emailAddress = document.getElementById('email');
-const navBottom = document.getElementById('nav-mobile-bottom');
-const navMiddle = document.getElementById('nav-right');
-const navTop = document.getElementById('nav-left');
-const projectCard = document.querySelectorAll(".left-project");
-const testimonials = document.getElementById('testimonials');
-const testReview = document.getElementById('test-right');
-const skillCard = document.querySelectorAll('.skill-square,.project-flex-box');
-
-let isDark = false;
+const navElements = document.querySelectorAll(' #nav-right, #nav-left');
+const navPopupElements = document.querySelectorAll('#nav-mobile-bottom');
+const cards = document.querySelectorAll('.project-flex-box,#testimonials,.skill-square');
+const cardText = document.querySelectorAll('.project-title,.mobile-desc,.desktop-desc,.test-left,.card-body,.test-company-name,.skill-name,.tech-name');
+const techName = document.querySelectorAll('.tech-name');
+const testCard = document.getElementById('test-right');
 
 toggle.addEventListener('click', function () {
   this.classList.toggle('bi-sunset-fill');
-  isDark = !isDark;
-  body.style.background = isDark ? '#222229' : '#f5f5f5';
-  body.style.color = isDark ? 'white' : 'black';
-  body.style.transition = '2s';
-  navStickyBar.style.background = isDark ? '#222229' : '#f5f5f5';
-  heroSecondTitle.style.color = isDark ? '#c6c6c6' : 'black';
-  heroThirdTitle.style.color = isDark ? '#c6c6c6' : 'black';
-  emailAddress.style.color = isDark ? 'white' : 'black';
-  navBottom.style.background = isDark ? '#45455a' : '#E5E5E5';
-  navMiddle.style.background = isDark ? '#222229' : '#F5F5F5';
-  navTop.style.background = isDark ? '#222229' : '#f5f5f5';
-  testimonials.style.background = isDark ? '#45455a' : '#A3A3B2';
-  testReview.style.background = isDark ? '#3F3F50' : '#9292A0';
 
-  navLinks.forEach(link => link.style.color = isDark ? "white" : "black");
-  socialLinksHero.forEach(link => link.style.color = isDark ? "white" : "black");
-  projectCard.forEach(card => card.style.background = isDark ? "#45455a" : "#A3A3B2");
-  skillCard.forEach(card => card.style.background = isDark ? "#45455a" : "#A3A3B2");
+  if (this.classList.toggle('bi-brightness-high-fill')) {
+    body.style.background = '#f5f5f5';
+    body.style.color = 'black';
+    body.style.transition = '2s';
+    testCard.style.background = '#D6D6D6'
+    navStickyBar.style.background = '#f5f5f5';
+    heroTitles.forEach(title => title.style.color = 'black');
+    emailAddress.style.color = 'black';
+    navElements.forEach(nav => nav.style.background = '#f5f5f5');
+    navLinks.forEach(link => link.style.color = 'black');
+    cards.forEach(link => link.style.background = '#E5E5E5')
+    techName.forEach(link => link.style.color = 'black');
+    navPopupElements.forEach(link => link.style.background = '#E5E5E5')
+
+  } else {
+    body.style.background = '#222229';
+    body.style.color = 'white';
+    body.style.transition = '2s';
+    navStickyBar.style.background = '#222229';
+    heroTitles.forEach(title => title.style.color = '#c6c6c6');
+    emailAddress.style.color = 'white';
+    navElements.forEach(nav => nav.style.background = '#222229');
+    navLinks.forEach(link => link.style.color = 'white');
+    cards.forEach(link => link.style.background = '#262630')
+    navPopupElements.forEach(link => link.style.background = '#262630')
+    techName.forEach(link => link.style.color = 'white');
+
+
+  }
 });
 
 
